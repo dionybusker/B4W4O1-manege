35f8cb7cb9f8df036bf4bb4de54f7548c5072c79
Diony <dionybusker@gmail.com>
2020-06-09 21:34:39 +0200

    users create -> werkt

--- view/users/create.php
+++ view/users/create.php
@@ -1,14 +1,11 @@
 <h2>Registreren</h2>
 
-<form class="col-12" action="" method="POST">
-    <label class="col-5" for="username">Gebruikersnaam</label>
-    <input class="col-5" type="text" name="username">
+<form class="col-12" action="<?php echo URL ?>users/store" method="POST">
+    <label class="col-5" for="name">Naam</label>
+    <input class="col-5" type="text" name="name">
 
     <label class="col-5" for="fullname">Volledige naam</label>
     <input class="col-5" type="text" name="fullname">
-
-    <label class="col-5" for="password">Wachtwoord</label>
-    <input class="col-5" type="password" name="password">
 
     <label class="col-5" for="address">Adres</label>
     <input class="col-5" type="text" name="address">
@@ -16,7 +13,7 @@
     <label class="col-5" for="phone">Telefoonnummer</label>
     <input class="col-5" type="text" name="phone">
 
-    <input class="col-5" type="submit">
+    <input class="col-5" type="submit" value="Registreren!">
 
 
 </form>
