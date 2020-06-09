9b77bbe6debd3840473cd6c5eab1cf3f35dbd73f
Diony <dionybusker@gmail.com>
2020-06-09 21:34:13 +0200

    info users op index page

--- view/users/index.php
+++ view/users/index.php
@@ -1,2 +1,17 @@
-<a class="btn btn-outline-dark" href="<?=URL?>users/register">Registreren</a>
-<a class="btn btn-outline-dark" href="<?=URL?>users/login">Inloggen</a>
+<a class="btn btn-outline-dark" href="<?php echo URL ?>users/create">Bezoeker registreren</a>
+
+<div class="row col-12 justify-content-center">
+    <?php foreach ($users as $user) { ?>
+        <ul class="col-3 card navbar-nav m-2 p-0">
+            <li>Voornaam: <?php echo $user["name"] ?></li>
+            <li>Volledige naam: <?php echo $user["full_name"] ?></li>
+            <li>Adres: <?php echo $user["address"] ?></li>
+            <li>Tel. nummer: +31 <?php echo $user["phonenumber"] ?> </li> <br>
+            <li class="row m-0 p-0">
+                <a class="col-6 m-0 p-0 btn btn-info rounded-0" href="<?php echo URL ?>users/edit/<?php echo $user["id"] ?>">Gegevens bewerken</a>
+                <a class="col-6 m-0 p-0 btn btn-danger rounded-0" href="<?php echo URL ?>users/delete/<?php echo $user["id"] ?>">Gegevens verwijderen</a>
+            </li>
+
+        </ul>
+    <?php } ?>
+</div>
