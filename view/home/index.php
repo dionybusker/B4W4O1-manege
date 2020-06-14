7011fa45b0aed9ac9d15c3214b65bf099de67ad7
Diony <dionybusker@gmail.com>
2020-06-14 16:33:32 +0200

    extra tekst op de homepage

--- view/home/index.php
+++ view/home/index.php
@@ -1,4 +1,13 @@
 <div class="container">
-    <h1>Welkom op de manege.</h1>
-    <p>Je bent nu in home/index.</p>
+    <h1>Welkom op de manege!</h1>
+    <p>
+        Op deze website kunt u ruiters registreren, paarden toevoegen en reserveringen maken. <br>
+        Een paard reserveren kost € 55,- per uur. <br>
+        U kunt enkel een paard per uur reserveren, korter zal helaas niet mogelijk zijn.
+    </p>
+    <p>
+        Onder de knop "Ruiters" vindt u een overzicht van alle geregistreerde ruiters op de manege. Mocht u een ruiter willen toevoegen, aanpassen of verwijderen dan kan dat op de ruiterpagina. <br>
+        Onder de knop "Paarden" vindt u een overzicht van alle aanwezige paarden op de manege. Mocht u een paard willen toevoegen, aanpassen of verwijderen dan kan dat op de paardenpagina. <br>
+        Onder de knop "Reserveringen" vindt u een overzicht van alle huidige reserveringen op de manege. Mocht u een reservering willen maken, aanpassen of verwijderen dan kan dat op de reserveringenpagina. <br>
+    </p>
 </div>
